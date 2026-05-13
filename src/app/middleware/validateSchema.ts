91dd764db53c0bb471670a09adc0d643f4731b82
china akther <chinaakther05@gmail.com>
2026-05-13 10:05:48 +0600

    validate Schema

--- src/app/middleware/validateSchema.ts
+++ src/app/middleware/validateSchema.ts
@@ -1,12 +1,24 @@
-//validationSchema
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
 
-import { NextFunction, Request, Response } from "express"
-import z from "zod"
+const validateRequest = (schema: z.ZodTypeAny) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const result = schema.safeParse(req.body);
 
-cost validateRequest = (schema:z.ZodTypeAny) =>{
-    return (req: Request, res: Response, next: NextFunction)=>{
-        try{
-const result = schema.safeParse(req.body);
-        }
-    };
-}
+      if (!result.success) {
+        return res.status(400).json({
+          success: false,
+          message: result.error.issues[0].message,
+        });
+      }
+
+      req.body = result.data;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default validateRequest;
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,15 +3,15 @@ import authRoute from "../modules/auth/auth.route";
 
 const router = Router();
 
-// 🧠 auth module connect
+
 router.use("/auth", authRoute);
 
-// 🧪 test route
+//  route
 router.get("/", (req, res) => {
   console.log("API HIT")
   res.json({
     success: true,
-    message: "API is working 🚀",
+    message: "API is working ",
   });
 });
 
